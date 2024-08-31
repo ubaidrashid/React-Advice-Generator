033eb7623796c31b8d0c50b69f9f3496822a1ee5
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2024-08-31 12:21:04 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [advice, setAdvice] = useState('Advice...')
+  const [counter, setCounter] = useState(0)
+  let arr = ['#3c6997', '#fe9000', '#E6E8E6', '#FDCA40', '#DF2935', '#3772FF', '#DF2935', '#FDCA40']
+  async function increa() {
+    let api = await fetch('https://api.adviceSlip.com/advice');
+    let json = await api.json()
+    console.log(json.slip.advice)
+    setAdvice(json.slip.advice)
+    setCounter(counter + 1)
+    let cont = document.querySelector('.advice-container');
+   cont.style.backgroundColor = arr[Math.ceil(Math.random()*8)] 
+   cont.style.color = 'black';
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Advice Count <span>{counter}</span></h1>
+      <h1 className='advice-container' >{advice}</h1>
+      <button onClick={increa}>Generate Advice</button>
     </div>
   );
 }
